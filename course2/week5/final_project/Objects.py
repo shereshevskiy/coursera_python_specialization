303d413aa79d28b7eb9815f2411b71ae48764591
shereshevskiy <d.shereshevskiy@gmail.com>
2019-04-03 14:15:36 +0300

    create the project after windows update

--- course2/week5/final_project/Objects.py
+++ course2/week5/final_project/Objects.py
@@ -69,6 +69,23 @@ class Hero(Creature):
             self.calc_max_HP()
             self.hp = self.max_hp
 
+class Eneme(Creature):  # fixme to finish
+
+    def __init__(self, stats, icon):
+        pos = [1, 1]
+        self.level = 1
+        self.exp = 0
+        self.gold = 0
+        super().__init__(icon, stats, pos)
+
+    def level_up(self):
+        while self.exp >= 100 * (2 ** (self.level - 1)):
+            yield "level up!"
+            self.level += 1
+            self.stats["strength"] += 2
+            self.stats["endurance"] += 2
+            self.calc_max_HP()
+            self.hp = self.max_hp
 
 class Effect(Hero):
 
--- course2/week5/final_project/ScreenEngine.py
+++ course2/week5/final_project/ScreenEngine.py
@@ -36,6 +36,7 @@ class GameSurface(ScreenHandle):
 
     def connect_engine(self, engine):
         # FIXME save engine and send it to next in chain
+        pass
 
     def draw_hero(self):
         self.game_engine.hero.draw(self)
@@ -93,6 +94,7 @@ class ProgressBar(ScreenHandle):
 
     def connect_engine(self, engine):
         # FIXME save engine and send it to next in chain
+        pass
 
     def draw(self, canvas):
         self.fill(colors["wooden"])
@@ -173,8 +175,8 @@ class InfoWindow(ScreenHandle):
     # draw next surface in chain
 
     def connect_engine(self, engine):
-        # FIXME set this class as Observer to engine and send it to next in
-        # chain
+        # FIXME set this class as Observer to engine and send it to next in chain
+        pass
 
 
 class HelpWindow(ScreenHandle):
@@ -196,6 +198,7 @@ class HelpWindow(ScreenHandle):
 
     def connect_engine(self, engine):
         # FIXME save engine and send it to next in chain
+        pass
 
     def draw(self, canvas):
         alpha = 0
